fbd9716e4df3ca10cf29c31da5b72083215de8a5
Virginie <virginie.alpinehouse@gmail.com>
2021-02-19 14:01:24 -0800

    styled button with reactstrap & fontawesome

--- src/components/MainSearch.js
+++ src/components/MainSearch.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Input, Button, InputGroup, InputGroupAddon } from 'reactstrap';
 
 const MainSearch = (props) => {
     return (
         <div className="row">
             <form onSubmit={props.handleSubmit}>
-                <div>
-                    <input placeholder="Search..." onChange={props.handleSearchInput} type="text" />
-                </div>
+                <InputGroup>
+                    <Input placeholder="Search..." onChange={props.handleSearchInput} />
+                    <InputGroupAddon addonType="append"><Button color="warning" type="submit"><i className="fa fa-arrow-circle-right fa-lg" /></Button></InputGroupAddon>
+                </InputGroup>
             </form>
         </div>
     )
